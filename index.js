c5ae743acac2c248972c18559672796bd04ba3cd
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-11-06 18:37:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(Captilizedsentence);
+  
 }
+function Captilizedsentence(str) {
+  let sentences = str.split(' ');
+  for (let i = 0; i < sentences.length; i++) {
+    let sentence = sentences[i];
+    sentences[i] = sentence.charAt(0).toUpperCase() + sentence.slice(1);
+  }
+  return sentences.join(' ');
+}
+
